812ed6fd08d92355b47bdd665f9fc50ff3730cca
Yihyun <hihyun16@gmail.com>
2021-01-21 23:47:38 +0900

    Add solution of 4673 - [4673. 셀프 넘버] 문제 풀이

---
+++ python/4673.py
@@ -0,0 +1,10 @@
+constructor = set()
+for A in range(10):
+    for B in range(10):
+        for C in range(10):
+            for D in range(10):
+                constructor.add(1001 * A + 101 * B + 11 * C + 2 * D)
+
+numbers = set([i for i in range(1, 10001)])
+for i in sorted(numbers - constructor):
+    print(i)
