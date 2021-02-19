8b1f882a474975f401a9fc9d591f2546114d2a4e
Yihyun <hihyun16@gmail.com>
2021-02-19 16:17:23 +0900

    Add solution of 9663 - [9663. N-Queen] 문제 풀이 - 파이썬은 시간 초과가 뜨기 때문에 추후 다른 언어로 풀이

---
+++ python/backtracking/9663.py
@@ -0,0 +1,28 @@
+def dfs(n, selected):
+    global answer
+    current = len(selected)
+    if current == n:
+        answer += 1
+        return
+    candidate = [i for i in list(range(n)) if i not in selected]  # 선택하지 않은 열 중
+    for i in range(current):  # 대각선으로 잡히는 경우 제외
+        distance = current - i
+        if selected[i] + distance in candidate:
+            candidate.remove(selected[i] + distance)
+        if selected[i] - distance in candidate:
+            candidate.remove(selected[i] - distance)
+    for i in candidate:  # 각 후보에 대한 유망성 탐색
+        dfs(n, selected[::] + [i])
+
+
+n = int(input())
+result = 0
+for i in range(n // 2):
+    answer = 0
+    dfs(n, [i])
+    result += (2 * answer)
+if n % 2 != 0:
+    answer = 0
+    dfs(n, [n // 2])
+    result += answer
+print(result)
