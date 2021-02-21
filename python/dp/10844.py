ebb94d9ff9027922fa799f18de07b82531cc0f23
Yihyun <hihyun16@gmail.com>
2021-02-21 10:59:12 +0900

    Add solution of 10844 - [10844. 쉬운 계단 수] 문제 풀이

---
+++ python/dp/10844.py
@@ -0,0 +1,14 @@
+mod = 1000000000
+n = int(input())
+dp = [[0] * 10 for _ in range(n + 1)]
+for i in range(1, 10):
+    dp[1][i] = 1
+for i in range(2, n + 1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][j+1] % mod
+        elif j == 9:
+            dp[i][j] = dp[i-1][j-1] % mod
+        else:
+            dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod
+print(sum(dp[n]) % mod)
