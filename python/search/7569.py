b2e73c56e523659734b8ab6579bfef9f151b3adf
Yihyun <hihyun16@gmail.com>
2021-04-26 10:20:55 +0900

    Modify solution of 7569 - [7569. 토마토] 문제 풀이 수정 - 재채점 후 발생한 UnboundLocalError 해결 - 코드 가독성 증진

--- python/search/7569.py
+++ python/search/7569.py
@@ -7,37 +7,35 @@ dy = [1, -1, 0, 0, 0, 0]
 dz = [0, 0, 0, 0, 1, -1]
 
 
-def bfs(q, graph):
+def get_day(graph, done, yet):
+    if not done:
+        return -1
+    if not yet:
+        return 0
+    q = deque(done)
+    h, r, c = 0, 0, 0
     while q:
         h, r, c = q.popleft()
         for i in range(len(dx)):
             nh, nr, nc = h + dz[i], r + dy[i], c + dx[i]
-            if 0 <= nh < len(graph) and 0 <= nr < len(graph[0]) and 0 <= nc < len(graph[0][0]):
-                if graph[nh][nr][nc] == 0:
-                    graph[nh][nr][nc] = graph[h][r][c] + 1
-                    q.append((nh, nr, nc))
-    return graph[h][r][c]
+            if (nh, nr, nc) in yet:
+                graph[nh][nr][nc] = graph[h][r][c] + 1
+                yet.remove((nh, nr, nc))
+                q.append((nh, nr, nc))
+    if yet:
+        return -1
+    return graph[h][r][c] - 1
 
 
 m, n, h = map(int, input().split())
 graph = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
-q = deque()
+done = []
+yet = set()
 for i in range(h):
     for j in range(n):
         for k in range(m):
             if graph[i][j][k] == 1:
-                q.append((i, j, k))
-day = bfs(q, graph) - 1 if q else -1
-
-have_zero = False
-for i in range(h):
-    for j in range(n):
-        if 0 in graph[i][j]:
-            have_zero = True
-            break
-    if have_zero:
-        break
-if have_zero:
-    print(-1)
-else:
-    print(day)
+                done.append((i, j, k))
+            if graph[i][j][k] == 0:
+                yet.add((i, j, k))
+print(get_day(graph, done, yet))
