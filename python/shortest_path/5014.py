4b003b2e0d57dad14d8988df66851bd7b413c797
YIHYUN HA <59721541+hyh1016@users.noreply.github.com>
2021-05-05 14:54:35 +0900

    Add solution of 5014
    - [5014. 스타트링크] 문제 풀이

---
+++ python/shortest_path/5014.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+F, S, G, U, D = map(int, input().split())
+shortest = [-1] * (F+1)
+q = [(0, S)]
+while q:
+  cost, index = heapq.heappop(q)
+  if shortest[index] != -1 and cost >= shortest[index]:
+    continue
+  shortest[index] = cost
+  if index+U <= F:
+    heapq.heappush(q, (cost+1, index+U))
+  if index-D >= 1:
+    heapq.heappush(q, (cost+1, index-D))
+print(shortest[G] if shortest[G] != -1 else "use the stairs")
