3509974efd0a546ae22977ad9e840bc010853d4b
Yihyun <hihyun16@gmail.com>
2021-09-03 22:16:48 +0900

    Update git manager - 에러 메시지를 조금 더 친절하게 수정 - 콘솔을 통해 동작 단계를 세부 안내
    Issue #1

--- python/git_manager.py
+++ python/git_manager.py
@@ -2,10 +2,10 @@ import urllib.request
 import re
 import git
 
-ERROR_READ_PROBLEM = "ERROR: fail to read problem"
-ERROR_GET_REPOSITORY = "ERROR: fail to get local repository"
-ERROR_GIT_COMMIT = "ERROR: fail to commit"
-ERROR_GIT_PUSH = "ERROR: fail to push"
+ERROR_READ_PROBLEM = "READ ERROR: 문제를 읽어오지 못하였습니다."
+ERROR_GET_REPOSITORY = "REPOSITORY ERROR: 로컬 리포지토리를 읽어오지 못하였습니다."
+ERROR_GIT_COMMIT = "COMMIT ERROR: 커밋을 수행하지 못하였습니다. 해당 파일이 있는지 확인해주세요."
+ERROR_GIT_PUSH = "PUSH ERROR: 원격 리포지토리에 반영하지 못하였습니다."
 
 
 def get_title(number):
@@ -42,6 +42,7 @@ def commit(repository, file_name, commit_message):
     try:
         repository.git.add(f"*/{file_name}")
         repository.git.commit(f"*/{file_name}", message=commit_message)
+        print(f"커밋이 완료되었습니다.")
     except:
         print(ERROR_GIT_COMMIT)
         exit()
@@ -50,6 +51,7 @@ def commit(repository, file_name, commit_message):
 def push(repository):
     try:
         repository.git.push("origin", "master")
+        print("원격 레포지토리에 반영되었습니다.")
     except:
         print(ERROR_GIT_PUSH)
         # reset commit
@@ -62,5 +64,9 @@ number = int(input())
 title = get_title(number)
 
 commit_message = get_commit_message(title, number)
-
-commit_and_push(f"{number}.py", commit_message)
+print(f"문제정보: [{number}. {title}]")
+is_commit_and_push = input("커밋하시겠습니까? [y/n]: ")
+if is_commit_and_push == 'y' or is_commit_and_push == 'Y':
+    commit_and_push(f"{number}.py", commit_message)
+else:
+    print("동작이 취소되었습니다.")
