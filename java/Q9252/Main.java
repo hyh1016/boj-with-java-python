a18cf11ce61120bdde1fbaaa43fa0f4b695d683d
YIHYUN HA <hihyun16@gmail.com>
2022-12-18 15:46:50 +0900

    Add solution of 9252 - [9252. LCS 2] 문제 풀이

--- java/Q9252/Main.java
+++ java/Q9252/Main.java
@@ -3,7 +3,7 @@ package Q9252;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Example Commit");
+        System.out.println("Example Commit!");
     }
 
 }
